306735b4a9f230c1326734b6d117a120eae897dc
Anmol Varma <anmol5varma@gmail.com>
2024-07-21 00:05:00 +0530

    fix: add support for full stop to change line.

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const addHeaderToImage = async (baseImage, { text, length }) => {
     const { textImage, textHeight } = await createCustomFont({ text, color, fontType: FONTS.roboto_black[100] });
 
     // Add 2mins read
-    const twoMinRead = await createCustomFont({ text: `${Math.ceil(length / 4)} mins read`, color: blackColor, fontType: FONTS.satisfy[32] });
+    const twoMinRead = await createCustomFont({ text: `${Math.ceil(length / 500)} min${Math.ceil(length / 500) > 1 ? 's' : ''} read`, color: blackColor, fontType: FONTS.satisfy[32] });
     const swipe = await createCustomFont({ text: 'swipe', color: blackColor, fontType: FONTS.satisfy[32] });
 
     // Calculate text position (centered)
@@ -27,28 +27,28 @@ const addHeaderToImage = async (baseImage, { text, length }) => {
     baseImage.composite(swipe.textImage, baseImage.bitmap.width - swipe.textWidth - PADDING, y + 18)
 
     // Add text to the image
-    // baseImage.composite(textImage, PADDING, y);
     await Promise.all(
         text.split(' ')
             .map(async (line, idx) => addTitle({ baseImage, text: line.toUpperCase(), color: blackColor, y: idx * 125 + 150 }))
     )
+    // await writeWordWrap(baseImage, { text, font: FONTS.roboto_black[100], color: blackColor, size: 100, x: 50, y: 150 }, {marginRight: 250, yGap: 25, xGap: 20})
 
     return baseImage;
 }
 
-const writeWordWrap = async (baseImage, { text, font, color, size, x = 150, y = 300 }) => {
+const writeWordWrap = async (baseImage, { text, font, color, size, x = 150, y = 300 }, { marginRight = 150, yGap = Math.ceil(size / 8), xGap = Math.ceil(size / 4) }) => {
     const wordList = text.split(' ')
-    const contentWidth = IMAGE_WIDTH - 150
+    const contentWidth = IMAGE_WIDTH - marginRight
     let yAxisPointer = y
     let xAxisPointer = x
     for (let word of wordList) {
         const { textImage, textWidth, textHeight } = await createCustomFont({ text: word, color, fontType: font });
         if ((xAxisPointer + textWidth) > contentWidth) {
             xAxisPointer = x
-            yAxisPointer += size + Math.ceil(size / 8)
+            yAxisPointer += size + yGap
         }
         baseImage.composite(textImage, xAxisPointer, yAxisPointer)
-        xAxisPointer += textWidth + Math.ceil(size / 4)
+        xAxisPointer += textWidth + xGap
     }
     return yAxisPointer + size
 }
@@ -58,12 +58,16 @@ const addContentToImage = async (baseImage, page, idx, total) => {
     const color = { r: 0, g: 0, b: 0 }
     const blackColor = Color(HexColor.black).object()
 
-    let yAxis = await writeWordWrap(baseImage, { text: page.title, font: FONTS.roboto_black[64], color, size: 64 })
-    yAxis = await writeWordWrap(baseImage, { text: page.description, font: FONTS.roboto[32], color, size: 32, y: yAxis + 20 })
+    let yAxis = await writeWordWrap(baseImage, { text: page.title, font: FONTS.roboto_black[64], color, size: 64 }, {})
+    const points = page.description.split('. ')
+    yAxis += 10
+    for (let point of points) {
+        yAxis = await writeWordWrap(baseImage, { text: point + (point.endsWith('.') ? '' : '.'), font: FONTS.roboto[32], color, size: 32, y: yAxis + 20 }, {})
+    }
 
     // Add pagination
-    const indexString = await createCustomFont({ text: idx, color: blackColor, fontType: FONTS.roboto_black[64] });
-    const totalString = await createCustomFont({ text: '/' + total, color: blackColor, fontType: FONTS.roboto_black[64] })
+    const indexString = await createCustomFont({ text: idx, color: blackColor, fontType: FONTS.bebas[64] });
+    const totalString = await createCustomFont({ text: '/' + total, color: blackColor, fontType: FONTS.bebas[64] })
     baseImage.composite(indexString.textImage, PADDING, PAGINATION_PADDING)
     baseImage.composite(totalString.textImage, baseImage.bitmap.width - PADDING - totalString.textWidth, PAGINATION_PADDING)
 
@@ -83,7 +87,7 @@ const addFooter = async (baseImage) => {
     baseImage.composite(textImage, (DP_DIAMETER + PADDING + PADDING), y + 18);
 
 
-    let photo = await Jimp.read('./assets/anmol.jpeg')
+    let photo = await Jimp.read('./assets/me.jpeg')
     let circleMask = await Jimp.read('./assets/circular_mask.png')
     photo.resize(DP_DIAMETER, DP_DIAMETER)
     circleMask.resize(DP_DIAMETER, DP_DIAMETER)
@@ -94,6 +98,9 @@ const addFooter = async (baseImage) => {
     return baseImage;
 }
 
+//Add last page
+const addAuthorDetails = async () => { }
+
 const main = async (content) => {
     const bgColor = Color(HexColor.skyblue).hexa();
     const date = new Date()
@@ -102,14 +109,13 @@ const main = async (content) => {
             const baseImage = createImage({ bgColor, width: IMAGE_WIDTH, height: IMAGE_WIDTH })
             let image = await addFooter(baseImage)
             if (idx === 0)
-                image = await addHeaderToImage(image, { text: page.title, length: content.length })
+                image = await addHeaderToImage(image, { text: page.title, length: content.reduce((total, page) => total + page.title.length + page.description.length, 0) })
             else
                 image = await addContentToImage(image, page, stringifyNumbers(idx), stringifyNumbers(content.length - 1))
             await saveImage(image, `./output/${date.toDateString()}/${idx}.png`)
         })
     )
 }
-
 
 const content = [
     {
@@ -118,7 +124,7 @@ const content = [
         tip: ''
     }, {
         title: 'Understand the basics',
-        description: 'Before diving into coding, it’s important to understand the fundamental concepts of web development.',
+        description: 'Before diving into coding, it is important to understand the fundamental concepts of web development.',
         tip: ''
     }, {
         title: 'Learn the Core Technologies',
